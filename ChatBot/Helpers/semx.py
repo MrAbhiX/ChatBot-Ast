e866909b47d971a652b0be64848af77d1250b0e6
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-05-26 15:56:01 +0530

    Create semx.py

---
+++ ChatBot/Helpers/semx.py
@@ -0,0 +1,21 @@
+from pyrogram.types import Message
+from ChatBot import *
+
+async def chat_bot_toggle(message: Message, is_userbot: bool):
+    status = message.text.split(None, 1)[1].lower()
+    chat_id = message.chat.id
+    db = await check_chatbot()
+    db = db["userbot"] if is_userbot else db["bot"]
+    if status == "enable":
+        if chat_id not in db:
+            await add_chatbot(chat_id, is_userbot=is_userbot)
+            text = "Chatbot Enabled!"
+            return await eor(message, text=text)
+        await eor(message, text="ChatBot Is Already Enabled.")
+    elif status == "disable":
+        if chat_id in db:
+            await rm_chatbot(chat_id, is_userbot=is_userbot)
+            return await eor(message, text="Chatbot Disabled!")
+        await eor(message, text="ChatBot Is Already Disabled.")
+    else:
+        await eor(message, text="**Usage:**\n/chatbot [ENABLE|DISABLE]")
