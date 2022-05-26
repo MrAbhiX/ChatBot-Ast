857332ecbf9d121d07fa7b9e1b89fc233a11956a
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-05-26 18:31:08 +0530

    Update __main__.py

--- ChatBot/__main__.py
+++ ChatBot/__main__.py
@@ -1,9 +1,55 @@
-from asyncio import gather, sleep
-
-from pyrogram import filters
 from pyrogram.types import Message
+from pyrogram import filters
+from ChatBot.Database.Mongo import db
+from ChatBot.Database import (db,
+                              check_chatbot, 
+                              add_chatbot, 
+                              rm_chatbot,
+                              chatbot_group,
+                              
+                              )
+from ChatBot.Helpers import capture_error
 
-from ChatBot import *
+
+
+async def chat_bot_toggle(message: Message, is_userbot: bool):
+    status = message.text.split(None, 1)[1].lower()
+    chat_id = message.chat.id
+    db = await check_chatbot()
+    db = db["userbot"] if is_userbot else db["bot"]
+    if status == "enable":
+        if chat_id not in db:
+            await add_chatbot(chat_id, is_userbot=is_userbot)
+            text = "Chatbot Enabled!"
+            return await eor(message, text=text)
+        await eor(message, text="ChatBot Is Already Enabled.")
+    elif status == "disable":
+        if chat_id in db:
+            await rm_chatbot(chat_id, is_userbot=is_userbot)
+            return await eor(message, text="Chatbot Disabled!")
+        await eor(message, text="ChatBot Is Already Disabled.")
+    else:
+        await eor(message, text="**Usage:**\n/chatbot [ENABLE|DISABLE]")
+
+
+# Enabled | Disable Chatbot
+
+
+
+async def type_and_send(message: Message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id if message.from_user else 0
+    query = message.text.strip()
+    await message._client.send_chat_action(chat_id, "typing")
+    response, _ = await gather(lunaQuery(query, user_id), sleep(3))
+    await message.reply_text(response)
+    await message._client.send_chat_action(chat_id, "cancel")
+
+
+
+
+
+# FOR USERBOT
 
 
 @abhi.on_message(
