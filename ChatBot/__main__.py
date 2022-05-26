a512d8f01d46f7ace091407508d878034ec5d47e
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-05-26 18:10:48 +0530

    Update __main__.py

--- ChatBot/__main__.py
+++ ChatBot/__main__.py
@@ -1,6 +1,54 @@
-from ChatBot import abhi, LOG_GROUP_ID as semx
+from asyncio import gather, sleep
 
-async def start_bot():
-    await abhi.start()
-    print("[INFO]: BOT & USERBOT CLIENT STARTED !!")
-    await abhi.send_message(f"{semx}", "I'm ChatBot Message Me For Chat With Me")
+from pyrogram import filters
+from pyrogram.types import Message
+
+from ChatBot import *
+
+
+@abhi.on_message(
+    filters.command("chatbot", prefixes=USERBOT_PREFIX)
+    & ~filters.edited
+    & SUDOERS
+)
+@capture_error
+async def chatbot_status_ubot(_, message: Message):
+    if len(message.text.split()) != 2:
+        return await eor(message, text="**Usage:**\n.chatbot [ENABLE|DISABLE]")
+    await chat_bot_toggle(message, is_userbot=True)
+
+
+@abhi.on_message(
+    ~filters.me & ~filters.private & filters.text & ~filters.edited,
+    group=chatbot_group,
+)
+@capture_error
+async def chatbot_talk_ubot(_, message: Message):
+    db = await check_chatbot()
+    if message.chat.id not in db["userbot"]:
+        return
+    username = "@" + str(USERBOT_USERNAME)
+    if message.reply_to_message:
+        if not message.reply_to_message.from_user:
+            return
+        if (
+                message.reply_to_message.from_user.id != USERBOT_ID
+                and username not in message.text
+        ):
+            return
+    else:
+        if username not in message.text:
+            return
+    await type_and_send(message)
+
+
+@abhi.on_message(
+    filters.text & filters.private & ~filters.me & ~filters.edited,
+    group=(chatbot_group + 1),
+)
+@capture_error
+async def chatbot_talk_ubot_pm(_, message: Message):
+    db = await check_chatbot()
+    if message.chat.id not in db["userbot"]:
+        return
+    await type_and_send(message)
