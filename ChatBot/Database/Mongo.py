eec2322c2cc3a0716b0ffe7820929e4e9cd6b1a9
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-05-26 17:03:23 +0530

    Update Mongo.py

--- ChatBot/Database/Mongo.py
+++ ChatBot/Database/Mongo.py
@@ -1,6 +1,11 @@
 from ChatBot import MONGO_URL
-from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
 
-mongo_client = MongoClient(MONGO_URL)
-db = mongo_client.wbb
 
+class Database:
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
+
+ 
