ebeab52e9b16afa2c4059db9f954f530887bc6d4
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-05-26 16:11:17 +0530

    Update __main__.py

--- ChatBot/__main__.py
+++ ChatBot/__main__.py
@@ -1,1 +1,6 @@
+from ChatBot import abhi, LOG_GROUP_ID as semx
 
+async def start_bot():
+    await abhi.start()
+    print("[INFO]: BOT & USERBOT CLIENT STARTED !!")
+    await abhi.send_message(f"{semx}", "I'm ChatBot Message Me For Chat With Me")
