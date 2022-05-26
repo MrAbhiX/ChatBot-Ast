9952e6fc50958af6e0784d7c18949a0f01ad12e0
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-05-26 16:20:41 +0530

    Update main.py

--- ChatBot/main.py
+++ ChatBot/main.py
@@ -9,17 +9,18 @@ from pyrogram.types import Message
 LOG_GROUP_ID = int(environ.get("LOG_GROUP_ID", None))
 API_ID = int(environ.get("API_ID", 6))
 API_HASH = environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-SESSION_STRING = environ.get("SESSION_STRING", None)
+SESSION_NAME = environ.get("SESSION_NAME", None)
 USERBOT_PREFIX = environ.get("USERBOT_PREFIX", ".")
 MONGO_URL = environ.get("MONGO_URL", None)
 ARQ_API_URL = environ.get("ARQ_API_URL", None)
 ARQ_API_KEY = environ.get("ARQ_API_KEY", None)
 
 abhi = Client(
-    SESSION_STRING,
     api_id=API_ID,
     api_hash=API_HASH,
-      )
+    session_name=SESSION_NAME,
+    
+    )
 
 SUDOERS = filters.user()
 
