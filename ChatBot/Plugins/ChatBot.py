b7159d592193061a4791f713967ff34670e50382
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-05-26 18:53:22 +0530

    Update ChatBot.py

--- ChatBot/Plugins/ChatBot.py
+++ ChatBot/Plugins/ChatBot.py
@@ -17,10 +17,7 @@ from ChatBot.main import (abhi,
                           USERBOT_PREFIX,
                           USERBOT_ID,
                           USERBOT_USERNAME,
-                          LOG_GROUP_ID,
-                          USERBOT_DC_ID,
-                          USERBOT_MENTION,
-                          USERBOT_NAME,
+                          
                           eor,
                           arq)
 
