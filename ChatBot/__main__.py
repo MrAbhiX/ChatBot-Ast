b7acc5bd91a8d1aa44a733d3a378374f2c2e8836
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-05-26 15:23:30 +0530

    Update __main__.py

--- ChatBot/__main__.py
+++ ChatBot/__main__.py
@@ -1,5 +1,17 @@
-from pyrogram import Client
+from pyrogram import Client, filters
+from aiohttp import ClientSession
+from Python_ARQ import ARQ
+from os import environ
+from inspect import getfullargspec
+import asyncio
 
+API_ID = int(environ.get("API_ID", 6))
+API_HASH = environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+SESSION_STRING = environ.get("SESSION_STRING", None)
+USERBOT_PREFIX = environ.get("USERBOT_PREFIX", ".")
+MONGO_URL = environ.get("MONGO_URL", None)
+ARQ_API_URL = environ.get("ARQ_API_URL", None)
+ARQ_API_KEY = environ.get("ARQ_API_KEY", None)
 
 abhi = Client(
     SESSION_STRING,
@@ -7,3 +19,31 @@ abhi = Client(
     api_hash=API_HASH,
       )
 
+SUDOERS = filters.user()
+
+lakshu = abhi.get_me()
+
+USERBOT_ID = lakshu.id
+USERBOT_NAME = lakshu.first_name + (lakshu.last_name or "")
+USERBOT_USERNAME = lakshu.username
+USERBOT_MENTION = lakshu.mention
+USERBOT_DC_ID = lakshu.dc_id
+
+if USERBOT_ID not in SUDOERS:
+    SUDOERS.add(USERBOT_ID)
+    
+aiohttpsession = ClientSession()
+
+arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
+
+async def eor(msg: Message, **kwargs):
+    func = (
+        (msg.edit_text if msg.from_user.is_self else msg.reply)
+        if msg.from_user
+        else msg.reply
+    )
+    spec = getfullargspec(func.__wrapped__).args
+    return await func(**{k: v for k, v in kwargs.items() if k in spec})
+
+print("ChatBot Assistant Started By Abhi")
+abhi.start()
